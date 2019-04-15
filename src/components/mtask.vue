91029e9560c30f94e9f666cbec5ac6abf17ccb74
benjaminfff <990460889@qq.com>
2019-04-15 19:12:05 +0800

    mtask finished

--- src/components/mtask.vue
+++ src/components/mtask.vue
@@ -7,7 +7,11 @@
           </el-button>
         </el-col>
       </div>
-      <div class="l-mtag-table">
+      <div class="l-mtag-table" style="position: relative">
+        <div
+          style="position: absolute;left: 0;top: 0;width: 100%;height: 100%;display: flex;justify-content:center;align-items: center;font-size: 2rem"
+          v-if="taskQueue.length==0">赞无任务
+        </div>
         <el-row class="l-mtag-table__header">
           <el-col :span="2">设备ID</el-col>
           <el-col :span="4">设备内存</el-col>
@@ -19,12 +23,15 @@
         <el-scrollbar class="mtag-el-scrollbar">
           <div class="l-mtag-table__body">
             <el-row v-for="task in taskQueue" class="l-mtag-table__tr">
-              <el-col :span="2">设备ID</el-col>
-              <el-col :span="4">设备内存</el-col>
-              <el-col :span="4">设备电量</el-col>
-              <el-col :span="6">资源名称</el-col>
-              <el-col :span="4">下发进度</el-col>
-              <el-col :span="4" style="text-align: center">操作</el-col>
+              <el-col :span="2">{{task.Device.Id}}</el-col>
+              <el-col :span="4">{{task.Storage}}</el-col>
+              <el-col :span="4">{{task.PowerState}}</el-col>
+              <el-col :span="6">{{task.Resource.Name}}</el-col>
+              <el-col :span="4">{{getDownLoadStateText(task)}}</el-col>
+              <el-col :span="4" style="text-align: center">
+                <el-button type="text" @click="deleteDeviceResource(task)">删除</el-button>
+                <el-button type="text" v-if="task.downloadState==2" @click="resendDownload(task)">重发</el-button>
+              </el-col>
             </el-row>
           </div>
         </el-scrollbar>
@@ -86,16 +93,30 @@
     },
     methods: {
       init() {
+        //localStorage.removeItem("taskQueue");
         let url = window.location.host;
-        this.adminSN = localStorage.getItem('adminSN') == null ? new Date().getTime() : localStorage.getItem('adminSN');
-        localStorage.setItem("adminSN",this.adminSN);
-        this.wsAdmin = new WebSocket("ws://"+url+"/ccweb/ws/join?sn=admin" + this.adminSN);
+        this.adminSN = localStorage.getItem('adminSN') == null ? "admin" + new Date().getTime() : localStorage.getItem('adminSN');
+        localStorage.setItem("adminSN", this.adminSN);
+        this.wsAdmin = new WebSocket("ws://" + url + "/ccweb/ws/join?sn=" + this.adminSN);
         this.wsAdmin.onmessage = (evt) => {
           console.log(evt.data);
           this.updateTaskQueue(JSON.parse(evt.data));
           this.wsAdmin.send('OK');
         }
         this.taskQueue = localStorage.getItem('taskQueue') == null ? [] : JSON.parse(localStorage.getItem('taskQueue'));
+      },
+      getDownLoadStateText(task) {
+        //0正在下载，1等待下载，2失败重发，3下载完成
+        switch (task.downloadState) {
+          case 0:
+            return task.DownloadProgress + "%";
+          case 1:
+            return "等待下载";
+          case 2:
+            return "失败";
+          case 3:
+            return "下载完成"
+        }
       },
       async fetchData() {
         await this.axios.get("/ccweb/api/resources/list").then((res) => {
@@ -104,10 +125,35 @@
             resource.selected = false;
           })
           this.resources = resources;
+          console.log(this.resources);
         });
         await this.axios.get("/ccweb/api/devices/list").then((res) => {
-          this.devices = res.data;
+          let devices = res.data;
+          devices.forEach((device) => {
+            device.connected == false;
+          });
+          this.devices = devices;
         });
+        setTimeout(() => {
+          /*this.wsAdmin.send(JSON.stringify({
+            Type: 2,
+            Data: {
+              Admin: this.adminSN,
+              UserDevice: this.devices[3],
+              Command: 3,
+              Resource: this.resources[1]
+            }
+          }))*/
+          /*this.wsAdmin.send(JSON.stringify({
+            Type: 2,
+            Data: {
+              Admin: this.adminSN,
+              UserDevice: this.devices[1],
+              Command: 2,
+              Resource: this.resources[1]
+            }
+          }))*/
+        }, 3000);
       },
       getSizeStrByByte(byteSize) {
         if (byteSize <= 1024) {
@@ -120,86 +166,146 @@
       },
       createTask() {
         let taskQueue = [];
-        this.devices.forEach((device) => {
-          this.resources.forEach((resource) => {
-            if (resource.selected) {
-              taskQueue.push({
-                Device: device,
-                Resource: resource,
-                Storage: "xx",
-                PowerState: "xx",
-                downloadState: 1,           //0正在下载，1等待下载，2失败重发，3下载完成
-                DownloadProgress: "xx"
-              });
-            }
-          });
+        this.resources.forEach((resource) => {
+          if (resource.selected) {
+            this.devices.forEach((device) => {
+              if (device.connected) {
+                taskQueue.push({
+                  Device: device,
+                  Resource: resource,
+                  Storage: "xx",
+                  PowerState: "xx",
+                  downloadState: 1,           //0正在下载，1等待下载，2失败重发，3下载完成
+                  DownloadProgress: "xx"
+                });
+              }
+            });
+          }
         });
-        this.taskQueue.push(...taskQueue);
+        taskQueue.forEach((task) => {
+          //已经有这个任务了
+          let hasTask = this.taskQueue.find((preTask) => {
+            return (preTask.Device.Id == task.Device.Id && preTask.Resource.Id == task.Resource.Id);
+          }) != undefined;
+          if (!hasTask) {
+            this.taskQueue.push(task);
+          }
+        })
         this.taskQueue.sort((task1, task2) => {
           return task1.downloadState - task2.downloadState;
         })
-        localStorage.setItem("taskQueue", JSON.parse(this.taskQueue));
+        localStorage.setItem("taskQueue", JSON.stringify(this.taskQueue));
         let curTaskCount = this.taskQueue.length < 5 ? this.taskQueue.length : 5;
         for (let i = 0; i < curTaskCount; i++) {
           if (this.taskQueue[i].downloadState == 1) {
-            this.wsAdmin.send({
+            this.wsAdmin.send(JSON.stringify({
               Type: 2,
               Data: {
                 Admin: this.adminSN,
-                UserDevice: this.taskQueue.Device,
+                UserDevice: this.taskQueue[i].Device,
                 Command: 2,
-                Resource: this.Resource
+                Resource: this.taskQueue[i].Resource
               }
-            })
+            }))
           }
         }
+        this.addDialogVisible = false;
+      },
+      deleteDeviceResource(task) {
+        this.wsAdmin.send(JSON.stringify({
+          Type: 2,
+          Data: {
+            Admin: this.adminSN,
+            UserDevice: task.Device,
+            Command: 3,
+            Resource: task.Resource
+          }
+        }))
+        this.taskQueue.forEach((mTask, index) => {
+          if (mTask === task) {
+            this.taskQueue.splice(index, 1);
+          }
+        });
+        localStorage.setItem('taskQueue', JSON.stringify(this.taskQueue));
+      },
+      resendDownload(task) {
+        this.wsAdmin.send(JSON.stringify({
+          Type: 2,
+          Data: {
+            Admin: this.adminSN,
+            UserDevice: task.Device,
+            Command: 2,
+            Resource: task.Resource
+          }
+        }))
+        task.downloadState = 1;
+      },
+      MBToGB(MB) {
+        return (MB / 1024).toFixed(2);
       },
       updateTaskQueue(res) {
         if (res.Type == 3) {
           let UserEvents = res.Data.UserEvents;
+          UserEvents.forEach((userEvent) => {
+            this.devices.forEach((device) => {
+              if (device.Id == userEvent.UserDevice.Id) {
+                device.connected = true;
+              }
+            })
+          });
           this.taskQueue.forEach((task) => {
             UserEvents.forEach((userEvent) => {
               if (task.Device.Id == userEvent.UserDevice.Id) {
                 task.PowerState = userEvent.PowerState;
-                if (task.Resource.Id == userEvent.DownloadingResource.Id) {
-                  if (userEvent.DownloadingProgress == -1) {
+                if (userEvent.DownloadingResource != null && task.Resource.Id == userEvent.DownloadingResource.Id && task.downloadState != 3) {
+                  if (userEvent.DownloadProgress == -1) {
                     task.downloadState = 1;
-                    task.DownloadingProgress = userEvent.DownloadProgress;
+                    task.DownloadProgress = userEvent.DownloadProgress;
+                  } else if (userEvent.DownloadProgress == 100) {
+                    task.downloadState = 3;
+                    task.DownloadProgress = userEvent.DownloadProgress;
                   } else {
                     task.downloadState = 0;
-                    task.DownloadingProgress = userEvent.DownloadProgress;
+                    task.DownloadProgress = userEvent.DownloadProgress;
                   }
                 }
               }
             })
           });
         } else if (res.Type == 4) {
-          let UserEvents = res.Data.UserEvents;
+          let UserDevice = res.Data.UserDevice;
+          let Storage = "剩余" + this.MBToGB(res.Data.UsedStorage) + 'G/' + this.MBToGB(res.Data.TotalStorage) + 'G'
+          let Resources = res.Data.Resources;
+          let ErrorCode = res.Data.ErrorCode;
           this.taskQueue.forEach((task) => {
-            UserEvents.forEach((userEvent) => {
-              if (userEvent.UserDevice.Id == task.Device.Id) {
-                task.Storage = userEvent.UsedStorage + "/" + userEvent.TotalStorage;
-                if (task.Resource.Id == userEvent.DownloadingResource.Id) {
-                  userEvent.ErrorCode != 0 ? task.downloadState = 1 : null;
-                }
+            if (task.Device.Id == UserDevice.Id) {
+              task.Storage = Storage;
+              if (Resources != null) {
+                Resources.forEach((resource) => {
+                  if (resource.Id == task.Resource.Id) {
+                    ErrorCode < 0 ? task.downloadState = 2 : null;
+                    ErrorCode == 4 ? task.downloadState = 3 : null;
+                  }
+                });
               }
-            })
+            }
           });
         }
         let curTaskCount = this.taskQueue.length < 5 ? this.taskQueue.length : 5;
         for (let i = 0; i < curTaskCount; i++) {
           if (this.taskQueue[i].downloadState == 1) {
-            this.wsAdmin.send({
+            this.wsAdmin.send(JSON.stringify({
               Type: 2,
               Data: {
                 Admin: this.adminSN,
-                UserDevice: this.taskQueue.Device,
+                UserDevice: this.taskQueue[i].Device,
                 Command: 2,
-                Resource: this.Resource
+                Resource: this.taskQueue[i].Resource
               }
-            })
+            }))
           }
         }
+        localStorage.setItem("taskQueue", JSON.stringify(this.taskQueue));
       }
     }
   }
